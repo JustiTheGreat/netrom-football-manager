bf505bfd0678bebed45cc8f5a03486e989168660
JustiTheGreat <justinian.zaharia@gmail.com>
2023-07-05 15:44:52 +0300

    starting point

---
+++ src/main/java/com/netrom/netromfootballmanager/NetromFootballManagerApplication.java
@@ -0,0 +1,13 @@
+package com.netrom.netromfootballmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetromFootballManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetromFootballManagerApplication.class, args);
+	}
+
+}
